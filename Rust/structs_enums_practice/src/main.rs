21472629551a3e55458a27e14afb8aa9f8c8fbbc
Kemboi Cheruiyot <87800637+KolKemboi@users.noreply.github.com>
2024-09-27 08:58:18 +0300

    structs and enums practices

---
+++ Rust/structs_enums_practice/src/main.rs
@@ -0,0 +1,51 @@
+
+
+
+
+fn main() {
+    exe_1();
+}
+
+
+
+fn exe_2()
+{
+    struct BookInfo {
+        get_summary: String,
+        is_expensive: f64,
+    }
+
+
+}
+
+fn exe_1()
+{
+    struct Book{
+        title: String,
+        author: String,
+        pages: u32,
+        price: f64,
+    }
+
+    impl Book {
+        fn new(title: String, author: String, pages: u32, price:f64) -> Book
+        {
+           return  Book {
+                title,
+                author,
+                pages,
+                price,
+        };
+        }
+        
+        fn out_info(&self)
+        {
+            println!("{}, {}, {}, {}", self.title, self.author, self.pages, self.price)
+        }
+    }
+    
+
+    let new_book = Book::new("kol".to_string(), "col".to_string(), 10, 10.0);
+
+    new_book.out_info();
+}
